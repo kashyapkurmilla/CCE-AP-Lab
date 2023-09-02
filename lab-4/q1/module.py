9d5067827d1aec155cfb57447851a9fff7f642f4
Kashyap Kurmilla <124886947+kashyapkurmilla@users.noreply.github.com>
2023-09-02 15:13:35 +0530

    lab-4

---
+++ lab-4/q1/module.py
@@ -0,0 +1,11 @@
+import math
+
+
+def sinValue(n):
+    return math.sin(n)
+
+
+def sqrRoot(n):
+    return math.sqrt(n)
+
+
---
+++ lab-4/q1/q1.py
@@ -0,0 +1,13 @@
+import module as m
+
+
+def main():
+    while True :
+        n = int(input('Enter Number : '))
+        print('1.Square-root of ')
+        print('2.sine value of ')
+        print('3.Exs')
+        print('Enter choice')
+
+
+    
